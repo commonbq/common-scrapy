540427d711dea02539c3b4e0cf86d2943376d27a
Tri Nguyen <nvutri@gmail.com>
2026-02-18 12:35:21 +0700

    Add project-wide proxy middleware using settings.PROXY

--- common/middlewares.py
+++ common/middlewares.py
@@ -5,6 +5,8 @@
 
 from scrapy import signals
 
+from common.settings import PROXY
+
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
@@ -54,47 +56,31 @@ class CommonSpiderMiddleware:
 
 
 class CommonDownloaderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
+    """Project-wide downloader middleware.
+
+    Enforces routing through settings.PROXY when configured.
+    """
 
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
 
     def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
+        # Force all outgoing requests through configured proxy.
+        if PROXY:
+            request.meta["proxy"] = PROXY
         return None
 
     def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
         return response
 
     def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
+        return None
 
     def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s" % spider.name)
+        if PROXY:
+            spider.logger.info("Spider opened: %s (proxy enabled)", spider.name)
+        else:
+            spider.logger.info("Spider opened: %s (proxy disabled)", spider.name)
--- common/settings/base.py
+++ common/settings/base.py
@@ -49,9 +49,9 @@ DOWNLOAD_DELAY = 1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "common.middlewares.CommonDownloaderMiddleware": 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+   "common.middlewares.CommonDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
